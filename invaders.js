ac009daaa594ac9abd1361f48f1e17fa587b7b15
robclose <robert.close@gmail.com>
2025-08-30 23:21:24 +0100

    improvements

--- invaders.js
+++ invaders.js
@@ -5,13 +5,19 @@ const gameCtx = document.getElementById('game-canvas').getContext('2d');
 const hudCtx = document.getElementById('hud-canvas').getContext('2d');
 const gameWidth = document.getElementById('game-canvas').width;
 const gameHeight = document.getElementById('game-canvas').height;
+const canvas = document.getElementById('hud-canvas')
 let missiles = [];
 let guns = [];
+let particles = [];
 let timestamp = null;
 let gamePhase = 'start';
 let wave1;
 let missilestoFire;
 let hud;
+let result;
+let tables = [2, 3, 4, 5, 10];
+let missilesMax;
+let typedTimeStamp = 0;
 
 class HUD {
     constructor() {
@@ -43,6 +49,7 @@ class Alien {
         this.x = x;
         this.y = y;
         this.size = 10;
+        this.isDead = false;
     }
     update () {
         this.x += wave1.vx;
@@ -50,7 +57,12 @@ class Alien {
     }
 
     draw () {
-        gameCtx.fillStyle = 'red';
+        if (this.isDead) {
+            gameCtx.fillStyle = '#331400';
+        }
+        else {
+            gameCtx.fillStyle = 'red';
+        }
         gameCtx.fillRect(this.x - this.size * 0.5, this.y - this.size * 0.5, this.size, this.size);
     }
 }
@@ -104,14 +116,15 @@ class Missile {
     constructor (alien, x) {
         this.trail = [];
         this.position = {x: x, y: gameHeight - 50 };
-        this.velocity = {x: 0, y: 0};
+        this.velocity = {x: (Math.random() - 0.5), y: -3 * Math.random() - 1.5};
         this.trailLength = 20;
         if (alien) {
             this.target = alien;
             this.target.missile = this;
+            this.isDud = false;
         } else {
-          this.target = {x: randInt(wave1.leftmost, wave1.rightmost), 
-                        y: randInt(wave1.topmost, wave1.bottommost)};
+          this.target = null;
+          this.isDud = true;
           }
         this.hasHitTarget = false;
     }
@@ -128,15 +141,53 @@ class Missile {
         if (this.trail.length > this.trailLength) this.trail.pop();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-        let steer = this.steerVector();
-        let accel = 0.1; 
-        this.velocity.x += steer.x * accel + 2 * (Math.random() - 0.5);
-        this.velocity.y += steer.y * accel + 2 * (Math.random() - 0.5);
+        if (!this.isDud) {
+            let steer = this.steerVector();
+            let accel = 0.1; 
+            this.velocity.x += steer.x * accel + 2 * (Math.random() - 0.5);
+            this.velocity.y += steer.y * accel + 2 * (Math.random() - 0.5);
+        } else {
+            // this.velocity.x -= this.velocity.x * 0.02
+            this.velocity.y += 0.02; //gravity
+            missiles.filter( m => m.position.y > gameHeight - 50 && !m.hasHitTarget).forEach (m => {
+                m.hasHitTarget = true;
+                for (let i = 0;i < 6;i++) {
+                    particles.push(new Particle(m.position, '#006d12ff'))
+                }
 
-        if (this.distToTarget() < 5 && this.target.size) {
-            this.hasHitTarget = true;
-            wave1.aliens = wave1.aliens.filter (a => a !== this.target);
+            });
+
+
         }
+
+        if (!this.isDud && this.distToTarget() < 5) {
+            this.hasHitTarget = true;
+            this.target.isDead = true;
+            particles.push(new Particle(this.position, 'red', 0.1),
+                            new Particle(this.position, 'red', 0.1),
+                            new Particle(this.position, 'red', 0.1))
+            let liveAliens = wave1.aliens.filter (a => !a.isDead);
+            if (liveAliens.length == 0) {
+                if (result == 0) {
+                    for (let a of wave1.aliens) {
+                        setTimeout(() => {
+                            for (let i = 0; i<50 ; i++) {
+                                let green = Math.floor(Math.random() * 256);
+                                let red = Math.floor(Math.random() * 128 + 128);
+                                let blue = Math.floor(Math.random() * 128);
+                                let alpha = Math.random() * 0.3 + 0.7;
+                                particles.push(new Particle({x: a.x, y: a.y}, `rgba(${red}, ${green}, ${blue}, ${alpha})`, 0))
+                            }
+                            wave1.aliens = wave1.aliens.filter( b => b !== a);
+                            }, Math.random() * 3000);
+                            
+                        }
+                    } else {
+                        wave1.aliens.length = 0;
+                    }
+                }
+            }
+        
         
     }
     distToTarget () {
@@ -157,7 +208,7 @@ class Missile {
     }
     draw () {
         gameCtx.fillStyle = 'grey';
-        if (!this.hasHitTarget) gameCtx.fillRect(this.position.x, this.position.y, 4, 4);
+        if (!this.hasHitTarget) gameCtx.fillRect(this.position.x - 4, this.position.y - 4, 8, 8);
         let gradient = gameCtx.createLinearGradient(this.trail[0].x, this.trail[0].y, 
                             this.trail[this.trail.length-1].x, this.trail[this.trail.length-1].y);
         gradient.addColorStop(0, "rgba(255, 128, 0, 1)");   // solid orange
@@ -173,6 +224,48 @@ class Missile {
     }
 }
 
+class Particle {
+	constructor(position, colour, gravity = 0.02) {
+		this.position = {x: position.x, y: position.y};
+		this.vx = Math.random() * 2 - 1;
+		this.vy = Math.random() * 2 - 1;
+		this.colour = colour;
+        this.trail = [];
+        this.trailLength = 8;
+        this.gravity = gravity;
+		}
+
+	update () {
+		
+        this.trail.unshift({x: this.position.x, y: this.position.y});
+        if (this.trail.length > this.trailLength) this.trail.pop();
+		this.position.x += this.vx;
+		this.position.y += this.vy;
+		this.vy += this.gravity;
+        if (this.gravity < 0.015) this.gravity += 0.00005;
+
+        if (this.position.y > gameHeight) particles = particles.filter (p => p !== this)
+
+    }
+    draw () {
+
+		gameCtx.fillStyle = this.colour;
+        let gradient = gameCtx.createLinearGradient(this.trail[0].x, this.trail[0].y, 
+                            this.trail[this.trail.length-1].x, this.trail[this.trail.length-1].y);
+        gradient.addColorStop(0, this.colour); 
+        gradient.addColorStop(1, 'rgba(200, 255, 0, 0)'); 
+
+        gameCtx.strokeStyle = gradient;
+        gameCtx.beginPath();
+        gameCtx.moveTo(this.position.x, this.position.y);
+        for (let point of this.trail) {
+            gameCtx.lineTo (point.x, point.y);
+        }
+        gameCtx.stroke();
+
+	}
+} 
+
 function gameLoop(time) {
 
     hudCtx.clearRect(0,0,gameWidth, gameHeight);
@@ -185,15 +278,19 @@ function gameLoop(time) {
             for (let g = 0; g < 12; g++) {
                 guns.push((gameWidth - 100)/12 * g + 50);
                 }
+
+            setupHandlers();
             gamePhase = 'spawnWave';
             timestamp = time;
         break;
 
         case 'spawnWave':
-            wave1 = new Wave(Math.floor(Math.random() * 10) + 2,Math.floor(Math.random() * 10) + 2);
+            wave1 = new Wave(tables[Math.floor(Math.random() * tables.length)],Math.floor(Math.random() * 10) + 2);
             for (let a of wave1.aliens) {a.draw(); }
             addAnalysisHudElements();
+            missilesMax = 12 * tables[tables.length - 1] + 10;
             gamePhase = 'analysis'; 
+            particles.length = 0
             timestamp = time;
         break;
 
@@ -202,42 +299,46 @@ function gameLoop(time) {
             for (let a of wave1.aliens) {a.draw(); }
             wave1.update();
             hud.draw(time - timestamp);
-            if (time - timestamp > 10 * 1000) {gamePhase = 'playerChooses'}
             break;
 
         case 'playerChooses':
             hud.elements.length = 0;
-            missilestoFire = prompt('How many missiles?');
             timestamp = time;
+            result = missilestoFire - wave1.aliens.length;
             gamePhase = 'firing';
             break;
 
         case 'firing':
-                if (wave1.aliens.length == 0 && missiles.filter( m => !m.hasHitTarget).length == 0) {
+                if (result == 0 && wave1.aliens.length == 0) {
                 gamePhase = 'end'
                 timestamp = time
                 hud.addElement(0, 3000, (dt) => {
                     hudCtx.fillText("You saved planet Earth!!", gameWidth * 0.5, gameHeight * 0.5)
                 });
+                break;
             }
-            else if (wave1.aliens.length == 0 && missiles.filter( m => !m.hasHitTarget).length > 0) {
+            else if (result > 0 && missilestoFire == 0 && missiles.length == 0) {
                 gamePhase = 'end'
                 timestamp = time
                 hud.addElement(0, 3000, (dt) => {
-                    hudCtx.fillText("Stray missiles destroy the Earth!!", gameWidth * 0.5, gameHeight * 0.5)
+                    hudCtx.fillText(`${result} stray missiles destroyed the Earth!!`, gameWidth * 0.5, gameHeight * 0.5)
                 });
+                break;
             }
-            else if (wave1.aliens.length > 0 && missiles.length == 0 && missilestoFire == 0) {
+            else if (result < 0 && missiles.length == 0 && missilestoFire == 0) {
                 gamePhase = 'end'
                 timestamp = time
                 hud.addElement(0, 3000, (dt) => {
-                    hudCtx.fillText(`${wave1.aliens.length} aliens survived to invade the Earth!!`, gameWidth * 0.5, gameHeight * 0.5)
+                    hudCtx.fillText(`${-result} aliens survived to invade the Earth!!`, gameWidth * 0.5, gameHeight * 0.5)
                 });
+                break;
             }
             for (let a of wave1.aliens) {a.update(); }
             for (let a of wave1.aliens) {a.draw(); }
             for (let m of missiles) {m.update();}
             for (let m of missiles) {m.draw();}
+            for (let p of particles) {p.update();}
+            for (let p of particles) {p.draw();}
             wave1.update(); 
             if (missilestoFire > 0 && time - timestamp > 50) {
                 timestamp = time;
@@ -253,42 +354,32 @@ function gameLoop(time) {
                 for (let a of wave1.aliens) {a.draw(); }
                 for (let m of missiles) {m.update();}
                 for (let m of missiles) {m.draw();}
+                for (let p of particles) {p.update();}
+                for (let p of particles) {p.draw();}
                 wave1.update(); 
-                if (time - timestamp > 5000) {gamePhase = 'spawnWave'}
+                if (time - timestamp > 5000 && particles.length == 0) {gamePhase = 'spawnWave'}
 
     }
 
     requestAnimationFrame(gameLoop);
 } 
 
-
 requestAnimationFrame(gameLoop);
-
-let codeScroll = [
-'0x7C11 11001001  vector<aliens> fleet_matrix[rows][columns]',
-'0xFF02 00011101  formation=GRIDLOCK  integrity=0x3E',
-'0xAC34 10100011  anomaly_detected? analysis::pending',
-'0x3D77 11101000  calc(rows*columns)=0b01100000',
-'0x84F1 01011110  aliens.position[x,y] => formation.align()',
-'0x920C 10011001  analysis_subroutine(rows, columns)',
-'0x4B5E 11100110  fleet_density=0x7A  formation: STABLE',
-'0x1F22 10110011  alien formation: EXPANDING',
-'0xA442 01011001  analysis: recalibrate_vectors()',
-'0x39B0 11101100  aliens.signal[rows][columns] = ACTIVE',
-'0x4E92 00110011  formation.checksum=0x9F  status=VALID',
-'0x9B20 10110101  analysis -> status: INCOMPLETE',
-'0x7C11 11001001  vector<aliens> fleet_matrix[rows][columns]',
-'0xFF02 00011101  formation=GRIDLOCK  integrity=0x3E',
-'0xAC34 10100011  anomaly_detected? analysis::pending',
-'0x920C 10011001  analysis_subroutine(rows, columns)',
-'0x4B5E 11100110  fleet_density=0x7A  formation: STABLE'
-]
 
 function randInt(lower, higher) {
     return Math.floor(Math.random() * (higher + 1 - lower) + lower)
 }
 
 function addAnalysisHudElements () {
+
+        hud.addElement(1000, Infinity, () => {
+            if (missilestoFire) hudCtx.fillText(missilestoFire, gameHeight * 0.5, gameHeight - 50);
+            hudCtx.strokeRect(50, gameHeight - 30, gameWidth - 250, 10);
+            hudCtx.fillRect(50, gameHeight - 30, missilestoFire / missilesMax * (gameWidth - 250), 10 );
+            hudCtx.strokeRect(gameWidth - 150, gameHeight - 40, 100, 30);
+            hudCtx.fillText("Fire!", gameWidth - 100, gameHeight - 23);
+        
+        });
             // Flashing rect
          hud.addElement(1500, 3000, (dt) => {
             if (Math.floor(dt/200) % 2 == 0 ) {
@@ -307,24 +398,6 @@ function addAnalysisHudElements () {
             }
         });
 
-        //Scrolling text
-        hud.addElement(1500, 4200, (dt) => {
-            hudCtx.textAlign = "left"	;
-            hudCtx.font = "20px monospace";
-            hudCtx.fillText('Analysing alien formation...', 50, 430);
-            hudCtx.font = "8px monospace";
-            const endOfScroll = 2000;
-            const scrollSpeed = 50;
-            const linesPassed = Math.floor(dt / scrollSpeed);
-            let linesErased = 0;
-            if (dt > endOfScroll) {
-                linesErased = Math.floor((dt - endOfScroll) / scrollSpeed);
-            }
-            for (let i = 0; i < codeScroll.length - linesErased; i++) {
-                const idx = (i + linesPassed) % codeScroll.length;
-                hudCtx.fillText(codeScroll[idx], 50, 450 + i * 9);
-            }
-        });
         // Tick marks 
         hud.addElement(2000, Infinity, (dt) => {
             let colTickNum = Math.min(wave1.cols, Math.floor(dt/100));
@@ -362,4 +435,77 @@ function addAnalysisHudElements () {
             hudCtx.fillText(wave1.rows, wave1.leftmost - 40, middleY );
             hudCtx.fillText(wave1.cols, middleX, wave1.topmost - 40);
         });
+}
+
+function setupHandlers () {
+
+	let intervalId = null;
+	let eMouse = null;
+
+	canvas.addEventListener('mousemove', e => eMouse = e );
+
+	canvas.addEventListener ( "mousedown", () => {
+		if (eMouse) {
+			intervalId = setInterval( () => setMissilesChoice(eMouse), 40);
+		}
+	});
+
+	canvas.addEventListener('mouseup', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+  });
+
+  canvas.addEventListener('mouseleave', () => {
+    clearInterval(intervalId);
+    intervalId = null;
+  });
+
+    window.addEventListener('keydown', (e) => {
+
+        if (gamePhase !== 'analysis') return;
+
+        if (Number.parseInt(e.key) >= 0) {
+            if (e.timeStamp - typedTimeStamp < 5 * 1000 && missilestoFire) {
+                missilestoFire = missilestoFire.toString() + e.key.toString();
+            } else {
+                missilestoFire = e.key;
+            }
+        } else if (e.key == 'Backspace' && missilestoFire) {
+            missilestoFire = missilestoFire.toString().slice(0, -1);
+        } else if (e.key == 'Enter' && missilestoFire > 0) {
+            gamePhase = 'playerChooses'
+            return;
+        }
+        missilestoFire = Math.min(missilestoFire, missilesMax);
+        typedTimeStamp = e.timeStamp;
+        
+  });
+
+
+
+}
+function setMissilesChoice (eMouse) {
+
+    if (gamePhase !== 'analysis') return;
+    
+    const rect = canvas.getBoundingClientRect();
+    const x = eMouse.clientX - rect.left;    
+    const y = eMouse.clientY - rect.top;   
+    const min = 50;
+    const max = gameWidth - 200;
+
+    if (x > gameWidth - 150 && x < gameWidth - 50 
+        && y > gameHeight - 40 && y < gameHeight - 10
+        && missilestoFire > 0) {
+            gamePhase = 'playerChooses'
+            return;
+        } else if (x > min - 20 && x < max + 20) {
+
+        let number = missilesMax / (max - min) * (x - min)
+        number = Math.floor(number);
+        number = Math.min(number, missilesMax);
+        number = Math.max(1, number);
+        missilestoFire = number;
+        }
+    
 }
