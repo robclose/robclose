d9e1ce654db27601005ff4ffb0fda3e98cef9580
robclose <robert.close@gmail.com>
2025-10-11 22:36:10 +0100

    refactor to trailer class

--- towing.js
+++ towing.js
@@ -4,113 +4,112 @@ const ctx = document.getElementById('canvas').getContext('2d');
 const gameWidth = document.getElementById('canvas').width;
 const gameHeight = document.getElementById('canvas').height;
 let keys = [];
+let train1 = [];
 
 class Car {
     constructor (colour) {
-        this.frontwheel = new Pos(300, 300);
-        this.steering = 0;
         this.speed = 0.5;
         this.length = 40;
-        this.trailerLength = 80;
-        this.backwheel = this.frontwheel.addVec(this.length, 0);
-        this.hitch = this.backwheel.addVec(20, 0);
-        this.trailerWheel = this.backwheel.addVec(this.trailerLength, 0);
+        this.trailerLength = 60;
         this.width = 25;
         this.colour = colour;
+        this.frontAxle = new Axle(300, 300, 0, this.width, 0);
+        this.rearAxle = new Axle(300 - this.length, 300, 0, this.width, 0);
+        this.hitch = this.rearAxle.centre.addVec(10, 0);
     }
     draw () {
         ctx.strokeStyle = this.colour;
 
-        const w = this.wheels;
-        const t = this.carAngle;
-        const t2 = this.trailerAngle;
-
          // Draw the car box
         ctx.lineWidth = 3;
         ctx.beginPath();
-        ctx.moveTo(w.leftFront.x, w.leftFront.y);
-        ctx.lineTo(w.rightFront.x, w.rightFront.y);
-        ctx.lineTo(w.rightRear.x, w.rightRear.y);
-        ctx.lineTo(w.leftRear.x, w.leftRear.y);
-        ctx.lineTo(w.leftFront.x, w.leftFront.y);
-        ctx.moveTo(w.leftTrailer.x, w.leftTrailer.y);
-        ctx.lineTo(w.rightTrailer.x, w.rightTrailer.y);
-        ctx.moveTo(this.trailerWheel.x, this.trailerWheel.y);
-        ctx.lineTo(this.hitch.x, this.hitch.y);
+        ctx.moveTo(this.frontAxle.leftHub.x, this.frontAxle.leftHub.y);
+        ctx.lineTo(this.frontAxle.rightHub.x, this.frontAxle.rightHub.y);
+        ctx.lineTo(this.rearAxle.rightHub.x, this.rearAxle.rightHub.y);
+        ctx.lineTo(this.rearAxle.leftHub.x, this.rearAxle.leftHub.y);
+        ctx.lineTo(this.frontAxle.leftHub.x, this.frontAxle.leftHub.y);
         ctx.stroke();
-
         
-
-        let lf1 = w.leftFront.addVec(8, t + this.steering);
-        let lf2 = w.leftFront.addVec(-8, t + this.steering);
-
-        let rf1 = w.rightFront.addVec(8, t + this.steering);
-        let rf2 = w.rightFront.addVec(-8, t + this.steering);
-
-        let lr1 = w.leftRear.addVec(8, t);
-        let lr2 = w.leftRear.addVec(-8, t);
-
-        let rr1 = w.rightRear.addVec(8, t);
-        let rr2 = w.rightRear.addVec(-8, t);
-
-        let lt1 = w.leftTrailer.addVec(8, t2);
-        let lt2 = w.leftTrailer.addVec(-8, t2);
-
-        let rt1 = w.rightTrailer.addVec(8, t2);
-        let rt2 = w.rightTrailer.addVec(-8, t2);
-        
-        
+        // Draw the car wheels
         ctx.lineWidth = 6;
         ctx.beginPath();
-        ctx.moveTo(lf1.x, lf1.y);
-        ctx.lineTo(lf2.x, lf2.y);
-        ctx.moveTo(rf1.x, rf1.y);
-        ctx.lineTo(rf2.x, rf2.y);
-        ctx.moveTo(lr1.x, lr1.y);
-        ctx.lineTo(lr2.x, lr2.y);
-        ctx.moveTo(rr1.x, rr1.y);
-        ctx.lineTo(rr2.x, rr2.y);
-        ctx.moveTo(lt1.x, lt1.y);
-        ctx.lineTo(lt2.x, lt2.y);
-        ctx.moveTo(rt1.x, rt1.y);
-        ctx.lineTo(rt2.x, rt2.y);
+        ctx.moveTo(this.frontAxle.leftWheel[0].x, this.frontAxle.leftWheel[0].y );
+        ctx.lineTo(this.frontAxle.leftWheel[1].x, this.frontAxle.leftWheel[1].y );
+        ctx.moveTo(this.frontAxle.rightWheel[0].x, this.frontAxle.rightWheel[0].y );
+        ctx.lineTo(this.frontAxle.rightWheel[1].x, this.frontAxle.rightWheel[1].y );
+        ctx.moveTo(this.rearAxle.leftWheel[0].x, this.rearAxle.leftWheel[0].y );
+        ctx.lineTo(this.rearAxle.leftWheel[1].x, this.rearAxle.leftWheel[1].y );
+        ctx.moveTo(this.rearAxle.rightWheel[0].x, this.rearAxle.rightWheel[0].y );
+        ctx.lineTo(this.rearAxle.rightWheel[1].x, this.rearAxle.rightWheel[1].y );
         ctx.strokeStyle = "cornflowerblue";
         ctx.stroke();
 
     }
-    get carAngle () {
-        return Math.atan2(this.backwheel.y - this.frontwheel.y, 
-            this.backwheel.x - this.frontwheel.x);
-    }
-    get trailerAngle () {
-        return Math.atan2(this.trailerWheel.y - this.hitch.y, 
-            this.trailerWheel.x - this.hitch.x);
-    }
-    move () {
-        this.frontwheel = this.frontwheel.addVec(this.speed, this.carAngle + this.steering);
-        this.backwheel = this.frontwheel.addVec(this.length, this.carAngle + Math.PI);
-        this.hitch = this.backwheel.addVec(20, this.carAngle + Math.PI);
-        this.trailerWheel = this.hitch.addVec(this.trailerLength, this.trailerAngle + Math.PI);
 
-        this.steering *= 0.93;
+    move () {
+        this.frontAxle.centre = this.frontAxle.centre.addVec(this.speed, this.frontAxle.theta + this.frontAxle.steering);
+        
+        let t = Math.atan2(this.rearAxle.centre.y - this.frontAxle.centre.y, 
+            this.rearAxle.centre.x - this.frontAxle.centre.x);
+        this.rearAxle.theta = t;
+        this.frontAxle.theta = t;
+
+        this.rearAxle.centre = this.frontAxle.centre.addVec(this.length, t + Math.PI);
+        this.hitch = this.rearAxle.centre.addVec(10, t + Math.PI);
+        
+        this.frontAxle.steering *= 0.93;
     }
-    get wheels () {
-        let a = this.carAngle;
-        let a2 = this.trailerAngle
-        return {
-            leftFront: this.frontwheel.addVec(this.width * 0.5, a - Math.PI * 0.5),
-            rightFront: this.frontwheel.addVec(this.width * 0.5, a + Math.PI * 0.5),
-            leftRear: this.backwheel.addVec(this.width * 0.5, a - Math.PI * 0.5),
-            rightRear: this.backwheel.addVec(this.width * 0.5, a + Math.PI * 0.5),
-            leftTrailer: this.trailerWheel.addVec(this.width * 0.5, a2 - Math.PI * 0.5),
-            rightTrailer: this.trailerWheel.addVec(this.width * 0.5, a2 + Math.PI * 0.5),
-        }
-    }
+
     steerLeft() {
-        this.steering = Math.max(this.steering - 0.05, -0.5);
+        this.frontAxle.steering = Math.max(this.frontAxle.steering - 0.05, -0.5);
     }
     steerRight() {
-        this.steering = Math.min(this.steering + 0.05, 0.5);
+        this.frontAxle.steering = Math.min(this.frontAxle.steering + 0.05, 0.5);
+    }
+}
+
+class Trailer {
+    constructor(hitchedTo) {
+        this.length = 50;
+        this.width = hitchedTo.width;
+        this.colour = hitchedTo.colour;
+        this.hitchedTo = hitchedTo;
+        this.axle = new Axle(hitchedTo.hitch.x - this.length, 300, 0, this.width, 0);
+        this.hitch = this.axle.centre.addVec(10, 0);
+    }
+    move () {
+        
+        let t = Math.atan2(this.axle.centre.y - this.hitchedTo.hitch.y, 
+            this.axle.centre.x - this.hitchedTo.hitch.x);
+        this.axle.theta = t;
+
+        this.axle.centre = this.hitchedTo.hitch.addVec(this.length, t + Math.PI);
+        this.hitch = this.axle.centre.addVec(10, t + Math.PI);
+        
+        }
+        
+        draw () {
+        ctx.strokeStyle = this.colour;
+
+        // Draw the trailer box
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(this.axle.leftHub.x, this.axle.leftHub.y);
+        ctx.lineTo(this.axle.rightHub.x, this.axle.rightHub.y);
+        ctx.moveTo(this.axle.centre.x, this.axle.centre.y);
+        ctx.lineTo(this.hitchedTo.hitch.x, this.hitchedTo.hitch.y);
+        ctx.stroke();
+
+        // Draw the car wheels
+        ctx.lineWidth = 6;
+        ctx.beginPath();
+        ctx.moveTo(this.axle.leftWheel[0].x, this.axle.leftWheel[0].y );
+        ctx.lineTo(this.axle.leftWheel[1].x, this.axle.leftWheel[1].y );
+        ctx.moveTo(this.axle.rightWheel[0].x, this.axle.rightWheel[0].y );
+        ctx.lineTo(this.axle.rightWheel[1].x, this.axle.rightWheel[1].y );
+        ctx.strokeStyle = "cornflowerblue";
+        ctx.stroke();
+
     }
 }
 
@@ -126,26 +125,57 @@ class Pos {
 
 }
 
+class Axle {
+    constructor(x, y, theta, width, steering = 0) {
+        this.centre = new Pos (x, y);
+        this.theta = theta;
+        this.steering = steering;
+        this.offset = width * 0.5;
+    }
+    get leftHub () {
+        return this.centre.addVec(this.offset, this.theta - Math.PI * 0.5);
+    }
+    get rightHub () {
+        return this.centre.addVec(this.offset, this.theta + Math.PI * 0.5);
+    }
+    get leftWheel () {
+        return [this.leftHub.addVec(8, this.theta + this.steering),
+                this.leftHub.addVec(-8, this.theta + this.steering)];
+    }
+    get rightWheel () {
+        return [this.rightHub.addVec(8, this.theta + this.steering),
+                this.rightHub.addVec(-8, this.theta + this.steering)];
+    }
+
+}
+
+
 function gameLoop() {
 
     ctx.clearRect(0 ,0, gameWidth, gameHeight);
-    car1.move();
-    car1.draw();
+    train1.forEach( v => v.move());
+    train1.forEach( v => v.draw());
 
-    if (keys.includes('a')) { car1.steerLeft(); }
-    else if (keys.includes('d')) { car1.steerRight(); }
+    if (keys.includes('a')) { train1[0].steerLeft(); }
+    else if (keys.includes('d')) { train1[0].steerRight(); }
 
     if (keys.includes('w')) {
-        car1.speed = Math.min(car1.speed + 0.1, 5);
+        train1[0].speed = Math.min(train1[0].speed + 0.1, 5);
     }
       else if (keys.includes('s')) {
-        car1.speed = Math.max(car1.speed - 0.1, -5);
+        train1[0].speed = Math.max(train1[0].speed - 0.1, -5);
     }
   
     requestAnimationFrame(gameLoop);
 }
 
-let car1 = new Car('red');
+
+train1.push(new Car('red') );
+train1.push(new Trailer(train1[train1.length - 1]));
+train1.push(new Trailer(train1[train1.length - 1]));
+train1.push(new Trailer(train1[train1.length - 1]));
+train1.push(new Trailer(train1[train1.length - 1]));
+train1.push(new Trailer(train1[train1.length - 1]));
 requestAnimationFrame(gameLoop);
 
 window.addEventListener('keydown', (e) => {
